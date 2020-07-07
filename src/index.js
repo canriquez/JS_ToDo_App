5fc514cd4158504e3b1f3dcd9c9ba1280418054e
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-07 17:45:06 -0500

    added date functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('webpack-icons-installer');
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-
+import { differenceInCalendarDays, format } from 'date-fns'
 
 import { todoItem } from './components/todoitem';
 import { todoProject } from './components/todoproject'
@@ -21,9 +21,9 @@ const book = todoBook('user1');
 book.initialize();
 
 const p1 = todoProject('TODO APP');
-const item1 = todoItem('Define Data model', 'Create excel with data model and methods', '7/07/2020', 'high');
-const item2 = todoItem('Create basic Factories', 'Create factory files and methods', '8/07/2020', 'high');
-const item3 = todoItem('Create screens wireframes', 'Create initial screen designs', '9/07/2020', 'high');
+const item1 = todoItem('Define Data model', 'Create excel with data model and methods', '2020-07-07', 'high');
+const item2 = todoItem('Create basic Factories', 'Create factory files and methods', '2020-07-08', 'high');
+const item3 = todoItem('Create screens wireframes', 'Create initial screen designs', '2020-07-09', 'high');
 
 p1.addItem(item1);
 p1.addItem(item2);
@@ -43,14 +43,20 @@ function renderProjects() {
 }
 
 function renderItems(project) {
-    let htmlTag = `    <div class="list-group">
-    <div class="list-group-item list-group-item-action active">
-      Today
-    </div>`;
+    let htmlTagToday = ``;
+    let htmlTagTomorrow = ``;
+    let htmlTagLater = ``;
 
     let i = 0;
     for (let item of book.getSingleProject(project).getProjectItems()) {
-        htmlTag += `<div clas="projectItem" id="item${i}">
+
+        let date = Date.parse(item.getDueDate());
+        let todayDate = new Date();
+
+        let result = differenceInCalendarDays(date, todayDate);
+
+        if (result == 0) {
+            htmlTagToday += `<div clas="projectItem" id="item${i}">
                 <div class="list-group-item list-group-item-action">
                 <h3>${item.getTitle()}</h3>
                 <p>${item.getDescription()}</p>
@@ -58,12 +64,31 @@ function renderItems(project) {
                     <span id="remove${i}" class="glyphicon glyphicon-remove"></span>
                 </div>
             </div>`
+        } else if (result == 1) {
+            htmlTagTomorrow += `<div clas="projectItem" id="item${i}">
+            <div class="list-group-item list-group-item-action">
+            <h3>${item.getTitle()}</h3>
+            <p>${item.getDescription()}</p>
+                <span id="edit${i}" class="glyphicon glyphicon-pencil"></span>
+                <span id="remove${i}" class="glyphicon glyphicon-remove"></span>
+            </div>
+        </div>`
+        } else {
+            htmlTagLater += `<div clas="projectItem" id="item${i}">
+            <div class="list-group-item list-group-item-action">
+            <h3>${item.getTitle() + (result < 0? " - Overdue":" - Upcoming")}</h3>
+            <p>${item.getDescription()}</p>
+                <span id="edit${i}" class="glyphicon glyphicon-pencil"></span>
+                <span id="remove${i}" class="glyphicon glyphicon-remove"></span>
+            </div>
+        </div>`
+        }
         i += 1;
     }
 
-    htmlTag += `</div>`
-
-    document.getElementById('todoItems').innerHTML = htmlTag;
+    document.getElementById('today').innerHTML = htmlTagToday;
+    document.getElementById('tomorrow').innerHTML = htmlTagTomorrow;
+    document.getElementById('later').innerHTML = htmlTagLater;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
