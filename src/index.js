aa2d63bcfae1d5d1b5aca727e646bdbdd14014e0
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-08 15:04:06 -0500

    added projects listener and plus buttons

--- src/index.js
+++ src/index.js
@@ -27,8 +27,8 @@ const item2 = todoItem('Create basic Factories', 'Create factory files and metho
 const item3 = todoItem('Create screens wireframes', 'Create initial screen designs', '2020-07-09', 'high');
 
 p1.addItem(item1); // Item added into the project's array with the project Id Set.
-p1.addItem(item2);// Item added into the project's array with the project Id Set.
-p1.addItem(item3);// Item added into the project's array with the project Id Set.
+p1.addItem(item2); // Item added into the project's array with the project Id Set.
+p1.addItem(item3); // Item added into the project's array with the project Id Set.
 
 // book.addProject(p1);
 console.log(`project Name: ${p1.getName()}, project Id: ${p1.getProjectId()}`);
@@ -40,9 +40,11 @@ console.log(`id :${book.getProjects()[0].getProjectId()}`);
 function renderProjects() {
     let htmlTag = '';
     for (let i = 0; i < book.getProjects().length; i += 1) {
-        htmlTag += `<div id="p${i}"><p class="project">${book.getProjects()[i].getName()}</p></div>`;
+        htmlTag += `<div class="card projectItem" id="p${i}">
+        <p class="project card-body" data-index="${i}">${book.getProjects()[i].getName()}</p>
+        </div>`;
     }
-    document.getElementById('projects').innerHTML = htmlTag;
+    document.getElementById('projectItems').innerHTML = htmlTag;
 }
 
 function renderItems(project) {
@@ -51,7 +53,7 @@ function renderItems(project) {
     let htmlTagLater = '';
 
     let i = 0;
-    for (const item of book.getSingleProject(project).getProjectItems()) {
+    book.getSingleProject(project).getProjectItems().forEach(item => {
         const date = Date.parse(item.getDueDate());
         const todayDate = new Date();
 
@@ -110,7 +112,7 @@ function renderItems(project) {
         </div>`;
         }
         i += 1;
-    }
+    });
 
     document.getElementById('today').innerHTML = htmlTagToday;
     document.getElementById('tomorrow').innerHTML = htmlTagTomorrow;
@@ -119,5 +121,15 @@ function renderItems(project) {
 
 document.addEventListener('DOMContentLoaded', () => {
     renderProjects();
-    renderItems(1);
+    renderItems(0);
+
+    //add listener to projects
+    const project_items = document.getElementsByClassName('project');
+    const showListProjects = function showListProjects() {
+        console.log(this.getAttribute('data-index'));
+        renderItems(this.getAttribute('data-index'));
+    };
+    for (let i = 0; i < project_items.length; i += 1) {
+        project_items[i].addEventListener('click', showListProjects, false);
+    }
 });
