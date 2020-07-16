794dad0a7ab3c48d7c22e89241ecfaa7b9942b7e
enelesmai <xochitl.selene.fp@gmail.com>
2020-07-16 12:09:52 -0500

    added clear storage button

--- src/index.js
+++ src/index.js
@@ -4,46 +4,44 @@ import './style.css';
 import { todoBook } from './components/todobook';
 import { DisplayController } from './components/displaycontroller';
 import { Storage } from './components/storage';
-
-// navbar logo
 import logo from './icons/logo.svg';
-
 require('webpack-icons-installer');
-
-// Storage.clearStorage();
 
 const logoIcon = new Image();
 logoIcon.src = logo;
 document.getElementById('brandLogo').appendChild(logoIcon);
-
 const book = todoBook('user1');
 
 function initializeBookStorage() {
-  // / Book Storage Metod
-  if (Storage.getObjectStorage()) {
-    console.log('localStorage object present... reading now..');
-    book.convertJSONtoProjects(Storage.getObjectStorage());
-  } else {
-    console.log('localStorage object not present.. generating empty book');
-    book.initialize();
-    Storage.saveBook(book);
-  }
+    if (Storage.getObjectStorage()) {
+        book.convertJSONtoProjects(Storage.getObjectStorage());
+    } else {
+        book.initialize();
+        Storage.saveBook(book);
+    }
+}
 
-  console.log(`After reading local storage book : ${book.getSingleProject(0).getName()}`);
+function initApp() {
+    initializeBookStorage(); // Creates a new book or reads existing storage
+    book.setDomSelectedProject(0); // sets project default as selected;
+    DisplayController.renderNewItemForm();
+    DisplayController.prepareProjects(book);
+    DisplayController.addListenerProjectBtn(book);
+    DisplayController.addListenerItemBtn(book);
+}
+
+function clear() {
+    Storage.clearStorage();
+    book.clearProjects();
+    initApp();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(`BOOK IS THIS: ${book.getUser()}`);
+    initApp();
 
-  initializeBookStorage(); // Creates a new book or reads existing storage
-
-  book.setDomSelectedProject(0); // sets project default as selected;
-  DisplayController.renderNewItemForm();
-  DisplayController.prepareProjects(book);
-
-  // add listener to buttons
-  DisplayController.addListenerProjectBtn(book);
-  DisplayController.addListenerItemBtn(book);
+    document.getElementById('btnClearStorage').addEventListener('click', () => {
+        clear();
+    });
 });
 
 export default book;
